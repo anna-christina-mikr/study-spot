7fdb2589b49e4924cbe53e26adc8f4228baaeea6
anna-christina-mikr <a.micros19@gmail.com>
2026-02-04 10:13:08 +0200

    Remove files from tracking (now in .gitignore)

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// firebaseConfig.js
-export const firebaseConfig = {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'your-app.firebaseapp.com',
-    projectId: 'your-project-id',
-    storageBucket: 'your-app.appspot.com',
-    messagingSenderId: 'your-messaging-sender-id',
-    appId: 'your-app-id'
-  };
